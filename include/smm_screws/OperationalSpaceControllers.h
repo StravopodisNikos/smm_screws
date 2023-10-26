dbcc43725b1fa702182a67e028b6dd5698c776e7
nikos <stravnik.rob@gmail.com>
2023-10-26 12:49:31 +0300

    coding idosc class

--- include/smm_screws/OperationalSpaceControllers.h
+++ include/smm_screws/OperationalSpaceControllers.h
@@ -4,13 +4,64 @@
 #include <Eigen/Dense>
 #include <Eigen/Core>
 #include <boost/numeric/odeint.hpp>
+#include "smm_screws/robot_shared.h"
+#include <ros/ros.h>
+
+#define IDOSC_STATE_DIM 6
 
 namespace OperationalSpaceControllers {
 
 class InverseDynamicsController {
     public:
+        InverseDynamicsController();
+        InverseDynamicsController(ScrewsKinematics *ptr2kinematics, ScrewsDynamics *ptr2dynamics);
+
+        Eigen::Matrix<float, IDOSC_STATE_DIM, 1> error_state;
+        Eigen::Matrix<float, IDOSC_STATE_DIM, IDOSC_STATE_DIM> transition_matrix;
+        Eigen::Matrix<float, DOF, DOF> Kp;
+        Eigen::Matrix<float, DOF, DOF> Kd;
+
+        // Initialize functions
+        void initialize_gain_matrices();
+
+        // Set functions
+        //void update_current_state();
+        void set_desired_state(Eigen::Matrix<float, IDOSC_STATE_DIM, 1> desired_state_received);    // desired state is 'updated' between segments not execution steps
         
+        //void update_error_state();
+        void set_error_state(Eigen::Matrix<float, IDOSC_STATE_DIM, 1> current_state_received);
+        void set_state_transition_matrix();
+
+        // Update functions (functions that update matrices from outside class)
+        void update_control_input();
+        void update_torques(); // control output command -> torque command
+        
+
     private:
+        ScrewsKinematics *_ptr2_screws_kin_object;
+        ScrewsDynamics *_ptr2_screws_dyn_object;
+        float _kp;
+        float _kd;
+        Eigen::Matrix<float, DOF, DOF> _I_dof;
+        Eigen::Matrix<float, DOF, DOF> _O_dof;
+        Eigen::Matrix<float, DOF, DOF> _Kp;
+        Eigen::Matrix<float, DOF, DOF> _Kd;
+        Eigen::Matrix<float, DOF, 1> _x1;
+        Eigen::Matrix<float, DOF, 1> _x2;
+        Eigen::Matrix<float, IDOSC_STATE_DIM, 1> _X;
+        Eigen::Matrix<float, DOF, 1> _d1;
+        Eigen::Matrix<float, DOF, 1> _d2;
+        Eigen::Matrix<float, IDOSC_STATE_DIM, 1> _D;    // Desired state vector
+        Eigen::Matrix<float, DOF, 1> _y;                // control input vector
+        Eigen::Matrix<float, DOF, 1> _dq;
+        Eigen::Matrix<float, DOF, 1> _torque_cmd;
+        Eigen::Matrix3f _iJop;
+        Eigen::Matrix3f _dtJop;
+        
+        // Update functions
+        void update_dq(float *dq_new);
+        void update_inverse_operational_jacob();
+        void update_derivative_operational_jacob();
 };
 
 class ImpedanceController {
--- src/OperationalSpaceControllers.cpp
+++ src/OperationalSpaceControllers.cpp
@@ -3,4 +3,72 @@
 using namespace OperationalSpaceControllers;
 
 InverseDynamicsController::InverseDynamicsController() {};
+InverseDynamicsController::InverseDynamicsController(ScrewsKinematics *ptr2kinematics, ScrewsDynamics *ptr2dynamics):  _ptr2_screws_kin_object(ptr2kinematics), _ptr2_screws_dyn_object(ptr2dynamics) {
+    _I_dof = Eigen::Matrix<float, DOF, DOF>::Identity();
+    _O_dof = Eigen::Matrix<float, DOF, DOF>::Zero();
+    initialize_gain_matrices();
+    set_state_transition_matrix(); // not used yet
+}
 
+void InverseDynamicsController::initialize_gain_matrices() {
+    ros::NodeHandle nh;
+    
+    if (!nh.getParam("idosc/kp", _kp)) {
+        ROS_ERROR("[InverseDynamicsController/initialize_gain_matrices] Failed to load: Kp");
+        _Kp = Eigen::Matrix<float, DOF, DOF>::Zero();
+    } else {
+        _Kp = -_kp * _I_dof;
+    }
+    if (!nh.getParam("idosc/kd", _kd)) {
+        ROS_ERROR("[InverseDynamicsController/initialize_gain_matrices] Failed to load: Kp");
+        _Kd = Eigen::Matrix<float, DOF, DOF>::Zero();
+    } else {
+        _Kd = -_kd * _I_dof;
+    }
+    return;
+}
+
+void InverseDynamicsController::set_state_transition_matrix() {
+    transition_matrix.block<3, 3>(0, 0) = -_Kd;
+    transition_matrix.block<3, 3>(0, 3) = -_Kp;
+    transition_matrix.block<3, 3>(3, 0) = _I_dof;
+    transition_matrix.block<3, 3>(3, 3) = _O_dof;
+    return;
+}
+
+void InverseDynamicsController::set_desired_state(Eigen::Matrix<float, IDOSC_STATE_DIM, 1> desired_state_received) {
+    _D = desired_state_received;
+    return;
+}
+
+void InverseDynamicsController::set_error_state(Eigen::Matrix<float, IDOSC_STATE_DIM, 1> current_state_received) {
+    _X = _D - current_state_received;
+    _x1 = _X.block<DOF, 1>(0, 0); // velocity error
+    _x2 = _X.block<DOF, 1>(3, 0); // position error
+    return;
+}
+
+void InverseDynamicsController::update_dq(float *dq_new) {
+    for (size_t i = 0; i < DOF; i++) {_dq[i] = dq_new[i];}
+    return;
+}
+
+void InverseDynamicsController::update_inverse_operational_jacob() {
+    _iJop = _ptr2_screws_kin_object->Jop.inverse();
+    return;
+}
+
+void InverseDynamicsController::update_derivative_operational_jacob() {
+    _dtJop = _ptr2_screws_kin_object->dJop;
+    return;
+}
+
+void InverseDynamicsController::update_control_input() {
+    _y = _iJop * ( _Kd * _x1 + _Kp * _x2 - _dtJop * _dq);
+    return;
+}
+
+void InverseDynamicsController::update_torques() {
+    _torque_cmd = _ptr2_screws_dyn_object->MassMatrix() * _y + (_ptr2_screws_dyn_object->CoriolisMatrix() * _dq + _ptr2_screws_dyn_object->GravityVector() + _ptr2_screws_dyn_object->FrictionVector() ) ;
+    return;
+}
